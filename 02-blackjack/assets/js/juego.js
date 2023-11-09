f91661395d972797c5b83fab192cbd9c7f4cc450
MilthonB <milthonbor@gmail.com>
2023-11-08 19:11:24 -0700

    Obtener el valor de la carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -50,7 +50,23 @@ const pedirCarta = ()=>{
     return carta
 }
 
-pedirCarta();
+//pedirCarta();
 
+const valorCarta =( carta )=>{
+
+    //Extraer el numero de la carta
+
+    const valor = carta.substring(0,carta.length -1 );
+  
+    return isNaN(valor) ? 
+        (( valor === 'A' ) ? 11 : 10) 
+        : valor * 1   
+
+
+}
+
+const valor = valorCarta(pedirCarta())
+
+console.log({valor })
 
 
