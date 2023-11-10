bd42b0bb9ce4d731cf0134e58a6ab586470e8279
MilthonB <milthonbor@gmail.com>
2023-11-10 14:31:22 -0700

    Clasese

---
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -0,0 +1,31 @@
+
+
+
+
+// clases
+class Persona {
+
+    // Siempre usa using stric
+    // propiedades de clase
+    nombre = '';
+    codigo = '';
+    frase = '';
+
+    constructor( nombre='Sin nombre', codigo='Sin codigo',frase='Sin frase' ){
+        this.nombre = nombre 
+        this.codigo = codigo 
+        this.frase = frase 
+        console.log('Hola!');
+    }
+
+
+}
+
+
+// Si no envias informacion al contructor entonces el valor de la instacioa seran undifined
+const spiderman= new Persona('Peter Parket', 'Spdy', 'Hola soy el sorprendente hombre que ara;a');
+const airesman= new Persona('Antonio estrella', 'ironman', 'Yo soy ires man');
+
+
+
+
