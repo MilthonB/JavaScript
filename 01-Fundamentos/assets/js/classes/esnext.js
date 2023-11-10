9e35d07aecae9116fa739ab8066af8917740633c
MilthonB <milthonbor@gmail.com>
2023-11-10 16:03:28 -0700

    esnext

---
+++ 01-Fundamentos/assets/js/classes/esnext.js
@@ -0,0 +1,27 @@
+
+
+
+class Recatangulo {
+
+
+    #area = 0
+    
+    constructor( base = 0, altura = 0  ){
+
+        this.base = base;
+        this.altura = altura;
+
+        this.#area =  base *altura
+    }
+
+
+
+
+}
+
+
+const rectangulo  = new Recatangulo(10,15);
+
+
+
+console.log(rectangulo)
