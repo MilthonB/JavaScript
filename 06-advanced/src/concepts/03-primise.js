4466a314c08d9ed77e1c41c9e3dfe7818b0e34b7
Milthon Borboa <81788252+MilthonB@users.noreply.github.com>
2023-11-15 16:35:20 -0700

    Fortmas de contruir las proimesas

--- 06-advanced/src/concepts/03-primise.js
+++ 06-advanced/src/concepts/03-primise.js
@@ -34,6 +34,32 @@ export  const promisesComponent = (element) =>{
     const id1 = '5d86371f1efebc31def272e2'
     const id2 = '5d86371f1efebc31def272e2'
     //findHero( id1 ).then( superHero => renderHero(superHero))
+
+    //Forma tres
+    Promise.all([
+        findHero(id1),
+        findHero(id2)
+    ])
+    .then( ({ hero1, hero2 })  => {
+        renderTwoHeroes(hero1, hero2)
+    })
+    .catch( renderError )
+
+
+    //! Forma dos
+    let hero1;
+    findHero( id1 )
+        .then( hero => {
+            hero1 = hero
+            return findHero(id2)
+        }).then( hero2 => {
+            renderTwoHeroes( hero1, hero2 )
+        })
+        .catch(renderError)
+
+
+
+// forma uno
     findHero( id1 )
         .then( (hero1 => {
             findHero(id2)
