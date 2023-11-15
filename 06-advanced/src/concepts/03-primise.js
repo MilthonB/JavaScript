3d92234bc0cb84ebaa36e8470505d48d7d2b2f5e
Milthon Borboa <81788252+MilthonB@users.noreply.github.com>
2023-11-15 16:21:09 -0700

    promesas from hell

--- 06-advanced/src/concepts/03-primise.js
+++ 06-advanced/src/concepts/03-primise.js
@@ -22,10 +22,25 @@ export  const promisesComponent = (element) =>{
     }
 
 
+    const renderTwoHeroes = (hero1, hero2) =>{
+        element.innerHTML = `
+            <h3>${hero1.name}</h3>
+            <h3>${hero2.name}</h3>
+        `
+
+
+    }
+
     const id1 = '5d86371f1efebc31def272e2'
+    const id2 = '5d86371f1efebc31def272e2'
     //findHero( id1 ).then( superHero => renderHero(superHero))
     findHero( id1 )
-        .then( renderHero )
+        .then( (hero1 => {
+            findHero(id2)
+            .then(hero2 => {
+                renderTwoHeroes(hero1,hero2)
+            })
+        }))
         //.catch(error => renderError(error))
         .catch( renderError )
  
