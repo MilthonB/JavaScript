9d04e444e2b1a2897a7d9469a7f30b0b904e5e38
Milthon Borboa <81788252+MilthonB@users.noreply.github.com>
2023-11-16 10:39:42 -0700

    Peticiones quote de una api

--- 07-http-app/breakingbad/breakingbad-app.js
+++ 07-http-app/breakingbad/breakingbad-app.js
@@ -2,14 +2,15 @@
 
 
 /**
- * @returns { Object } Information
+ * @returns { Promise<Object> } queote Information
  */
-const fetchQuote = ()=>{
+const fetchQuote = async ()=>{
+
+    const res  = await fetch('https://api.breakingbadquotes.xyz/v1/quotes')
+    const data = await res.json()
 
 
-
-
-
+    return data[0]
 }
 
 
@@ -18,12 +19,42 @@ const fetchQuote = ()=>{
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingBadApp = (element)=>{
+export const BreakingBadApp = async (element)=>{
 
-    console.log('I am here')
+   
 
     document.querySelector('#app-title').innerHTML = 'Breaking bad' 
-    element.innerHTML = 'Loading...'
+    
+    
+    //const quote = await fetchQuote()
+    
+    const quoteLabel = document.createElement('blockquote')
+    const auhtorLabel = document.createElement('h3')
+    const nextWuoteBUtton = document.createElement('button')
+    nextWuoteBUtton.innerText = 'Next Quote'
+    
+    
+
+    
+    const renderQuote = (data)=>{
+        
+        quoteLabel.innerHTML =  data.quote
+        auhtorLabel.innerHTML = data.author
+        element.replaceChildren(quoteLabel, auhtorLabel, nextWuoteBUtton)
+
+
+    }
+
+    nextWuoteBUtton.addEventListener('click', async(event) => {
+        element.innerHTML = 'Loading'    
+        const data = await fetchQuote()
+        renderQuote(data)
+
+    })
+
+
+    fetchQuote()
+        .then(renderQuote)
 
 
 
